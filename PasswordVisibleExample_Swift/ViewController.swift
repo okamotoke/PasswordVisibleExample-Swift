e011209589749132278c60249797a9b82a9814f2
sakura <sakura@sakura.local>
2018-01-15 20:58:19 +0900

    Initial Commit

---
+++ PasswordVisibleExample_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PasswordVisibleExample_Swift
+//
+//  Created by sakura on 2018/01/15.
+//  Copyright © 2018年 Keisuke Okamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
