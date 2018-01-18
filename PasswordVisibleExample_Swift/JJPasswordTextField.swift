4d2999006396a93fe0693d47484b4e8c764f5d37
sakura <sakura@sakura.local>
2018-01-18 21:45:12 +0900

    first commit

---
+++ PasswordVisibleExample_Swift/JJPasswordTextField.swift
@@ -0,0 +1,21 @@
+//
+//  JJPasswordTextField.swift
+//  PasswordVisibleExample_Swift
+//
+//  Created by sakura on 2018/01/15.
+//  Copyright © 2018年 Keisuke Okamoto. All rights reserved.
+//
+
+import UIKit
+
+class JJPasswordTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
